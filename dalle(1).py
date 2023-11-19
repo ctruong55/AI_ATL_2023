407e4f3490e4b1971131b0f50c481fc2c6490ef9
Jake Chang <jc@lawn-10-91-0-94.lawn.gatech.edu>
2023-11-19 03:46:52 -0500

    V1 of dalle env file safety protocols

---
+++ dalle(1).py
@@ -0,0 +1,66 @@
+import ssl
+
+from openai import OpenAI
+import urllib.request
+import certifi
+from github import Github
+import requests
+import os
+from flask import Flask, request
+
+
+def imgGenerator(textprompt):
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=textprompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    image_url = response.data[0].url
+    return image_url
+
+
+def urlDownloader(url):
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    with urllib.request.urlopen(url, context=ssl_context) as r, open("house.png", 'wb') as out_file:
+        data = r.read()  # Read the data from the URL
+        out_file.write(data)
+
+
+
+
+
+
+#app = Flask(__name__)
+g = Github(os.environ["GITKEY"])
+
+
+#@app.route('/', methods=['GET', 'POST'])
+#def upload_image():
+    #if request.method == 'POST':
+imgurl = imgGenerator("I want a house that is beautiful for Cynthia to live in")
+response = requests.get(imgurl)
+file_path = "House.png"
+
+repo = g.get_repo("prajyaguru003/AIATL-2024")
+try:
+    contents = repo.get_contents(file_path, ref="main")
+    repo.delete_file(contents.path, "Delete image", contents.sha, branch="main")
+except Exception as e:
+    print("none exist")
+repo.create_file(file_path, "Add image", response.content, branch="main")
+
+        #return "Image uploaded successfully!"
+    #return '''
+    #<form method="post">
+    #  <input type="text" name="image_url" placeholder="Enter Image URL" />
+    #  <input type="submit" value="Upload">
+   # </form>
+    #'''
+
+
+#if __name__ == '__main__':
+#   app.run(debug=True)
